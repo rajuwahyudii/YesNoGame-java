53eb5fc0927409b6389e6e8e81d2f835640827f7
Rwhytm <rajuwahyudi842@gmail.com>
2022-08-05 14:46:34 +0700

    add ttree for binary tree

--- src/NonBinaryTree/Tree.java
+++ src/NonBinaryTree/Tree.java
@@ -3,6 +3,17 @@ package NonBinaryTree;
 import java.util.*;
 
 public class Tree {
+    private Node root;
 
+    public void setRoot(Node root){
+        this.root = root;
+    }
+
+    public boolean isEmpty(){
+        return (root ==null);
+    }
+    public void print(){
+        root.print("", true);
+    }
 
 }
